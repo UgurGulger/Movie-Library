5fe470e738005498e649616a5e59e2f8048dda4e
UgurGulger <ugur@gulger.com>
2019-03-25 18:15:15 +0300

    Deleting film(s) from UI

--- project.js
+++ project.js
@@ -3,6 +3,7 @@ const form = document.getElementById("film-form");
 const titleElement = document.querySelector("#title");
 const directorElement = document.querySelector("#director");
 const urlElement = document.querySelector("#url");
+const cardBody = document.querySelectorAll(".card-body")[1];
 
 //Starting UI Object
 const ui = new UI();
@@ -21,6 +22,9 @@ function eventListeners(){
         let films = storage.getFilmsFromStorage();
         ui.loadAllFilms(films);
     });
+
+    cardBody.addEventListener("click",deleteFilm);
+
 }
 function addFilm(e){
     const title = titleElement.value;
@@ -42,4 +46,12 @@ function addFilm(e){
     ui.clearInputs(titleElement,urlElement,directorElement);
 
     e.preventDefault();
+}
+
+function deleteFilm(e){
+
+    if (e.target.id ==="delete-film"){
+        ui.deleteFilmFromUI(e.target);
+    }
+
 }
